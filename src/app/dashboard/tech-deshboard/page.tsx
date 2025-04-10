2ae8613abd63a9edcd9f01df7b358108e9d7f419
Paulo Holanda <paulorfholanda@gmail.com>
2025-04-10 08:37:49 -0300

    ✨ feat: implementação das telas de dashboards separadas

---
+++ src/app/dashboard/tech-deshboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center min-h-screen p-4 bg-gray-100">
+      <h1 className="text-2xl font-bold">Tech Dashboard</h1>
+      <p className="text-gray-600">Welcome to the tech dashboard!</p>
+    </div>
+  );
+}
